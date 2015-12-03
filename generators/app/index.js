b635ead0428177d71a7368947551df196bd535c1
Kilhog <kilhog@protonmail.com>
2015-12-03 14:03:58 +0100

    askModel

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,39 +2,42 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var fs = require('fs');
+var _ = require('lodash');
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
+
+  askModel: function () {
     var done = this.async();
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the riveting ' + chalk.red('generator-colisea') + ' generator!'
+      'Welcome to the riveting ' + chalk.red('Colisea') + ' generator!'
     ));
 
+    var files = fs.readdirSync('config/yml');
+
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'checkbox',
+      name: 'ymlModels',
+      message: 'What model generate ?',
+      choices: _.reject(files, function(f) { return f == "index.php" })
     }];
 
     this.prompt(prompts, function (props) {
       this.props = props;
-      // To access props later use this.props.someOption;
-
       done();
     }.bind(this));
   },
 
   writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    //this.fs.copy(
+    //  this.templatePath('dummyfile.txt'),
+    //  this.destinationPath('dummyfile.txt')
+    //);
   },
 
   install: function () {
-    this.installDependencies();
+    //this.installDependencies();
   }
 });
